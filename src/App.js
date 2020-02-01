40d9e170dc9f9ea993ad7fa93a5fef02a5214652
David Poindexter <drpoindexter@gmail.com>
2020-02-01 14:14:40 -0500

    Style buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { NameProvider } from "./NameContext";
 import HelloDisplay from "./components/HelloDisplay";
+
+const AppWrapper = styled.div`
+  width: 300px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  font-size: 1.2rem;
+  margin-right: 5px;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+    background-color: #efefef;
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    background-color: #fff;
+  }
+`;
 
 const App = () => {
   const [name, setName] = useState("David");
@@ -17,11 +43,13 @@ const App = () => {
     setName("David");
   };
   return (
-    <NameProvider value={name}>
-      <HelloDisplay />
-      <button onClick={handleChangeName}>Change Name</button>
-      <button onClick={handleResetName}>Reset</button>
-    </NameProvider>
+    <AppWrapper>
+      <NameProvider value={name}>
+        <HelloDisplay />
+        <Button onClick={handleChangeName}>Change Name</Button>
+        <Button onClick={handleResetName}>Reset</Button>
+      </NameProvider>
+    </AppWrapper>
   );
 };
 
